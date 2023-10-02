7ecc0f1003fa5bfe97129a5d5b881f9cb02998eb
kadirkaradag <kadirkaradag0132@gmail.com>
2023-10-02 20:39:40 +0300

    EntityBase and Order class created.

---
+++ Ordering.Domain/Entities/Base/Entity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Entities.Base
+{
+    public abstract class Entity : IEntityBase
+    {
+        
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; protected set; }
+
+        public Entity Clone()  // ilgili entity ye eriştiğimizde örneğin order.clone dedğimizde yeni bir order nesnesi olustururuz. örnek amaclı. Entity den türeyen tüm sınıvlara bu method uygulanabilir.
+        {
+            return (Entity)this.MemberwiseClone();
+        }
+    }
+}
---
+++ Ordering.Domain/Entities/Base/IEntityBase.cs
@@ -0,0 +1,8 @@
+﻿namespace Ordering.Domain.Entities.Base
+{
+    public interface IEntityBase  //tüm entity lerde defaul olarak olmasını istediğim fieldlar
+    {
+        int Id { get; }
+
+    }
+}
