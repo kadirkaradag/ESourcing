039f593276423399d4ce82e28acfdd011dfca9ea
kadirkaradag <kadirkaradag0132@gmail.com>
2023-10-02 22:08:15 +0300

    AddInfrastructure for DependencyInjection created and added to startup.cs

---
+++ Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Infrastructure.Data;
+
+namespace Ordering.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            //inMemory geliştirirken bu kodları kullanıcaz. sql server a geçince de o kodları yazıcaz bunları comment e alıcaz.
+            services.AddDbContext<OrderContext>(opt => opt.UseInMemoryDatabase(databaseName: "InMemoryDb"),
+                ServiceLifetime.Singleton,
+                ServiceLifetime.Singleton
+            );
+
+            //Add Repositories.
+
+            return services;
+        }
+    }
+}
