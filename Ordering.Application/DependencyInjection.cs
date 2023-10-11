3d97c1d7c58039cb4f69a99c3be2aa934c1782e1
kadirkaradag <kadirkaradag0132@gmail.com>
2023-10-11 21:52:33 +0300

    OrderCreateHandler and mappings created.

--- Ordering.Application/DependencyInjection.cs
+++ Ordering.Application/DependencyInjection.cs
@@ -1,17 +1,31 @@
-﻿using FluentValidation;
+﻿using AutoMapper;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Mapper;
 using System.Reflection;
 
 namespace Ordering.Application
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApplication(this IServiceCollection services) 
+        public static IServiceCollection AddApplication(this IServiceCollection services) //her defasında gidip ESourcing.Order startup.cs inde değişiklik yapmamak icin burada Ordering.Application için ihtiyacım olan dependencyleri inject edip startup icinde AddApplication'u cağırıyorum. 
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            #region Configure Mapper
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.ShouldMapProperty = p => p.GetMethod.IsPublic || p.GetMethod.IsAssembly;
+                cfg.AddProfile<OrderMappingProfile>();
+            });
+
+            var mapper = config.CreateMapper();
+
+            #endregion
 
             return services;
         }
---
+++ Ordering.Application/Handlers/OrderCreateHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Commands.OrderCreate;
+using Ordering.Application.Responses;
+using Ordering.Domain.Entities;
+using Ordering.Domain.Repositories;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Handlers
+{
+    public class OrderCreateHandler : IRequestHandler<OrderCreateCommand, OrderResponse>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public OrderCreateHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<OrderResponse> Handle(OrderCreateCommand request, CancellationToken cancellationToken)
+        {
+            var orderEntity = _mapper.Map<Order>(request);   //mapping hataları almamak icin OrderMappingProfile class ı olusturuldu. DependencyInjectionda da bu OrderMappingProfile inject edildi.
+
+            if (orderEntity == null)            
+                throw new ApplicationException("Entity could not be mapped!");            
+
+            var order = await _orderRepository.AddAsync(orderEntity);
+            
+            var orderResponse = _mapper.Map<OrderResponse>(order);
+
+            return orderResponse;
+        }
+    }
+}
